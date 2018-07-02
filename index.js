b16444302cbee72c36e0ae14f5fae94aa258232d
Chely Ho <adept_inept@yahoo.com>
2018-07-02 23:47:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ class Route {
 
   blocksTravelled() {
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    horizontal = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.vertical))
 
+    return horizontal
   }
 }
